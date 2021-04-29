fd5507dab074b0fa9f0018fdb1a6cc1d0557cff6
Carlos E. La Torre <amautacelt@gmail.com>
2021-04-29 16:01:24 -0400

    update1

--- js/challenge.js
+++ js/challenge.js
@@ -3,6 +3,7 @@
 //    -- find the current number
 //    -- set timer to increment number
 
+let running = true;
 let number = 0;
 const likes = {};
 
@@ -19,9 +20,11 @@ const minusButton = el('minus');
 minusButton.addEventListener('click', decrementNumber);
 
 function updateNumber(value) {
-    const counter = el('counter');
-    number += value;
-    counter.innerText = number;
+    if (running) {
+        const counter = el('counter');
+        number += value;
+        counter.innerText = number;
+    }
 }
 
 function incrementNumber() {
@@ -37,26 +40,75 @@ function decrementNumber() {
 //  -- attatch an event handler to the button that:
 //     -- use the current number for a hash key
 //     -- increment that hash key value
+//  -- display the like values without repetition
 
 el('heart').addEventListener('click', incrementLike);
 
 function incrementLike() {
-    likes[number] = (likes[number] === undefined) ? 1 : likes[number] + 1;
+    if (running) {
+        likes[number] = likes[number] === undefined ? 1 : likes[number] + 1;
+
+    // if (likes[number] === undefined) {
+    //   likes[number] = 1;
+    // }
+    // else {
+    //   likes[number] += 1;
+    // }
+
     renderLikes();
+    }
 }
 
 function renderLikes() {
     const likeList = document.querySelector('.likes');
     likeList.innerHTML = '';
 
-    Object.keys(likes).sort((a,b) => a-b).forEach(key => {
-        const newLike = document.createElement('li');
-        newLike.innerText = `Likes for ${key}: ${likes[key]}`;
-        likeList.append(newLike);
-    })
+    const numbers = Object.keys(likes);
+    const sortedNumbers = numbers.sort((a, b) => a - b);
 
-    }
+  // Object.keys(likes)
+  //   .sort((a, b) => a - b)
+  //   .
+    sortedNumbers.forEach((key) => {
+    const newLike = document.createElement('li');
+    newLike.innerText = `Likes for ${key}: ${likes[key]}`;
+    likeList.append(newLike);
+    });
+}
 
+// 4. Pause button
+// 5. Resume button
+
+const pauseButton = el('pause');
+pauseButton.addEventListener('click', togglePause);
+
+function togglePause() {
+    running = !running;
+    pauseButton.innerText = running ? ' pause ' : ' resume ';
+}
+
+// 6. Adding comments
+// -- grab our comment form
+// -- add submit listener
+// -- on submit, add the comment to the page
+
+const commentForm = el('comment-form');
+commentForm.addEventListener('submit', submitComment);
+
+function submitComment(event) {
+    event.preventDefault();
+    addComment(event.target.comment.value);
+    event.target.comment.value = '';
+}
+
+function addComment(comment) {
+    const commentList = el('list');
+    const commentDiv = document.createElement('div');
+    commentDiv.innerText = comment;
+    commentList.append(commentDiv);
+}
+
+////
 
 function el(id) {
     return document.getElementById(id);
